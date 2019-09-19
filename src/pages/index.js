160fcbea41b944bb6e29db0504a6ffbc8db435b6
rfihtengolts <9-b-rinat@rambler.ru>
2019-09-19 16:06:20 +0300

    translates: jp for header/footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,17 @@ import Layout from "../components/layout";
 import SEO from "../components/seo";
 import styles from "./index.module.scss";
 import Body from "../components/body/body";
+import LangWrapper from "../components/LangWrapper";
 
 const IndexPage = () => (
-  <Layout>
-    <SEO title="Santiment's JP Magazine - Crypto’s Most Comprehensive API" />
-    <section className={styles.wrapper}>
-      <Body />
-    </section>
-  </Layout>
+    <LangWrapper>
+        <Layout>
+            <SEO title="Santiment's JP Magazine - Crypto’s Most Comprehensive API" />
+            <section className={styles.wrapper}>
+                <Body />
+            </section>
+        </Layout>
+    </LangWrapper>
 );
 
 export default IndexPage;
